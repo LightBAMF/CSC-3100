c2d7ce0a8cf0a3012fe2c89061eb8540c177d4f2
lightsredemption5@gmail.com <lightsredemption5@gmail.com>
2026-09-02 16:42:46 -0700

    Step 10 completed

--- react-frontend/src/MyApp.jsx
+++ react-frontend/src/MyApp.jsx
@@ -1,25 +1,9 @@
 // src/MyApp.jsx
 import React, {useState} from "react";
 import Table from "./Table";
+import Form from "./Form";
 
-const characters = [
-  {
-    name: "Charlie",
-    job: "Janitor",
-  },
-  {
-    name: "Mac",
-    job: "Bouncer",
-  },
-  {
-    name: "Dee",
-    job: "Aspring actress",
-  },
-  {
-    name: "Dennis",
-    job: "Bartender",
-  },
-];
+
 
 function MyApp() {
   const [characters, setCharacters] = useState([
@@ -39,8 +23,14 @@ function MyApp() {
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
     </div>
   );
+
+  function updateList(person) {
+  setCharacters([...characters, person]);
+ }
+
 }
 
 export default MyApp;
